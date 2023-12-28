8c0371d03bec9f902d64d21ab82c83876b2476a6
Yukishiro <yukishiro.fuyumura@gmail.com>
2023-12-28 14:53:46 -0500

    Agregando repositorio y servicio de carrera

--- src/main/java/com/Primer_Spring/app/repository/CarreraRepository.java
+++ src/main/java/com/Primer_Spring/app/repository/CarreraRepository.java
@@ -1,5 +1,10 @@
 package com.Primer_Spring.app.repository;
 
-public interface CarreraRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Primer_Spring.app.entity.Carrera;
+@Repository
+public interface CarreraRepository extends JpaRepository<Carrera, Long> {
 
 }
